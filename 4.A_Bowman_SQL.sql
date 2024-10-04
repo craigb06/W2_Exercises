808ed86620ea1cc9e5138324be8ef00961051fc0
Student <cbowman@appdev.yearup.org>
2024-10-04 15:57:50 -0500

    exercise 4.A incomplete

---
+++ 4.A_Bowman_SQL.sql
@@ -0,0 +1,4 @@
+INSERT INTO employeedata (FirstName, LastName, Role, PhoneNumber, Address, Email, Active)
+VALUES ('Craig', 'Bowman', 'CEO', '7735555656', '515 Random St', 'customercraig@mail.com', 'yes');
+
+SELECT * FROM restaurant.employeedata;
